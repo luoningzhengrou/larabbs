9e0bacd83ae9d5c8472d37a8f13d6f642c814871
Feng <920474772@qq.com>
2020-03-20 11:29:56 +0800

    分类模型和数据

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,7 +27,7 @@ class AuthServiceProvider extends ServiceProvider
 
         // 修改策略自动发现的逻辑
         Gate::guessPolicyNamesUsing(function ($modelClass){
-           // 动态反悔模型对应的策略名称，如：// 'App/Model/User' => 'App/Policies/UserPolicy'
+           // 动态返回模型对应的策略名称，如：// 'App/Model/User' => 'App/Policies/UserPolicy'
             return 'App\Policies\\' . class_basename($modelClass) . 'Policy';
         });
     }
