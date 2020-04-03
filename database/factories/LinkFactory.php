2c0d4e79a9925e16d562c71aa3deac0542f523cf
Feng <920474772@qq.com>
2020-04-03 18:07:14 +0800

    推荐

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -2,10 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Link;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Link::class, function (Faker $faker) {
     return [
         'title' => $faker->name,
         'link' => $faker->url,
