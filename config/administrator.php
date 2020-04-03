be6aa793035c4ab9fff4cf2f2e68974a125e954e
Feng <920474772@qq.com>
2020-04-03 17:23:44 +0800

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -82,6 +82,11 @@ return array(
             'roles',
             'permissions',
         ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
     ],
 
     /*
