e0fe2f01b1a69907c71d4c5e6c776d1cd721a8a0
Feng <Feng@hh.com>
2020-09-02 01:30:44 +0000

    频率限制

---
+++ config/api.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    // 接口频率限制
+    'rate_limits' => [
+        // 访问频率限制，次/分钟
+        'access' => env('RATE_LIMITS','60,1'),
+        // 登陆相关，次/分钟
+        'sign' => env('SIGN_RATE_LIMITS','10,1'),
+    ],
+];
